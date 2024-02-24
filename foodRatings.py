30c2d6e60365186d743842e043e40ba77c56b011
Andrew Ting <ating26@bu.edu>
2024-02-24 02:48:44 -0500

    completed the functions that update the database with average ratings

--- foodRatings.py
+++ foodRatings.py
@@ -20,10 +20,34 @@ class FoodRating:
     
     def updateAvg(self):
         conn = sqlite3.connect('./databases/avgFoodRating.db')
-        cursor = conn.cursor()
-        query = ''
+        cur = conn.cursor()
+        cur.execute('CREATE TABLE IF NOT EXISTS ratings (location TEXT, business_name TEXT, item TEXT, avg_rating REAL, num_inputs INTEGER)')
+        cur.execute('SELECT * FROM ratings WHERE location=? AND business_name=? AND item=?',(self.location, self.businessName, self.item))
+        selection = cur.fetchone()
+        if selection is None:
+            cur.execute('INSERT INTO ratings (location, business_name, item, avg_rating, num_inputs) VALUES (?,?,?,?,?)', (self.location, self.businessName, self.item, self.rating, 1))
+        else:
+            cur.execute('SELECT avg_rating FROM ratings WHERE location=? AND business_name=? AND item=?', (self.location, self.businessName, self.item))
+            avg = cur.fetchone()[0]
+            cur.execute('SELECT num_inputs FROM ratings WHERE location=? AND business_name=? AND item=?', (self.location, self.businessName, self.item))
+            num = cur.fetchone()[0]
+            avg = ((avg*num)+self.rating)/(num+1)
+            num += 1
+            cur.execute('UPDATE ratings SET avg_rating=?, num_inputs=? WHERE location=? AND business_name=? AND item=?', (avg,num,self.location, self.businessName, self.item))
+        conn.commit()
+        cur.close()
+        conn.close()
         
-        
+input = FoodRating('GSU', 'Basho', 'sushi', 5, 'it is good')
 
-input = FoodRating('GSU', 'Halal_Shack', 'rice_bowl', 5, 'it is good')
+conn = sqlite3.connect('./databases/avgFoodRating.db')
+cur = conn.cursor()
+cur.execute('SELECT * FROM ratings')
+print(cur.fetchall())
+""" #This clears out the db entries for testing
+cur.execute('DELETE FROM ratings')
+conn.commit()
+"""
+cur.close()
+conn.close()
         
