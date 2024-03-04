3cfe16dbddeadb4ded85769f39ca25cda7ef50b7
Andrew Ting <ating26@bu.edu>
2024-03-04 04:09:55 -0500

    added flask

--- app.py
+++ app.py
@@ -1,3 +1,9 @@
 #flask file to make login system secure
-import flask from Flask
+from flask import Flask, render_template
+#type 'python3 -m pip install flask' or 'pip install flask' to get flask installed
 
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('mainpage.html')
